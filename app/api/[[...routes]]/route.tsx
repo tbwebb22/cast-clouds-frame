2ef617bdc63c54a152cc8405cbf158a85f146fa2
Taylor Webb <tbwebb22@gmail.com>
2024-04-22 11:01:18 -0400

    Make frame work across token IDs

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -2,71 +2,33 @@
 
 import { Button, Frog, TextInput } from 'frog';
 import { devtools } from 'frog/dev';
-// import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next';
 import { serveStatic } from 'frog/serve-static';
 import { abi } from '@/app/abi';
 import { baseSepolia } from 'viem/chains'; 
 
 const app = new Frog({
-  // assetsPath: '/',
   basePath: '/api',
-  // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 });
 
-// Uncomment to use Edge Runtime
-// export const runtime = 'edge'
+const urlMap: Record<string, string> = {
+  0: 'https://i.imgur.com/0VNw4fC.png',
+  1: 'https://i.imgur.com/7khQFNx.png',
+}
 
-app.frame('/', (c) => {
+app.frame('/home/:id', (c) => {
+  const id = c.req.param('id');
+
+  console.log("start token ", id);
+
   return c.res({
-    action: '/finish',
-    image: "https://i.imgur.com/0VNw4fC.png",
+    action: `/finish/${id}`,
+    image: urlMap[id],
     imageAspectRatio: '1:1',
     intents: [
-      <Button.Link href="https://i.imgur.com/0VNw4fC.png">Hi-res Image</Button.Link>,
-      <Button.Transaction target="/mint/0">Mint</Button.Transaction>,
+      <Button.Transaction target={`/mint/${id}`}>Mint</Button.Transaction>,
     ],
   })
-});
-
-// app.frame('/', (c) => {
-//   return c.res({
-//     action: '/mintbutton',
-//     image: "https://i.imgur.com/0VNw4fC.png",
-//     imageAspectRatio: '1:1',
-//     intents: [
-//       <Button>Mint2</Button>,
-//     ],
-//   })
-// });
-
-app.frame('/mintbutton', (c) => {
-  const { buttonValue } = c;
-  console.log("mintbutton");
-  return c.res({
-    // image: `${process.env.NEXT_PUBLIC_SITE_URL}/cast-cloud-daos.png`,
-    image: "https://i.imgur.com/0VNw4fC.png",
-    imageAspectRatio: '1:1',
-    intents: [
-      <Button.Mint target="eip155:84532:0xc33D61f88f207D2c1E3E2e90cF389F9bfCb25C2a:0">Mint</Button.Mint>,
-    ]
-  })
-})
-
-app.frame('/finish', (c) => {
-  console.log("inside finish");
-  const { transactionId } = c;
-  console.log("transactionID: ", transactionId);
-  console.log("finish c: ", c);
-  return c.res({
-    image: (
-      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
-        Transaction ID: {transactionId}
-        Minted!
-      </div>
-    )
-  });
 });
 
 app.transaction('/mint/:id', (c) => {
@@ -74,9 +36,6 @@ app.transaction('/mint/:id', (c) => {
 
   console.log("minting token ", id);
 
-  // return new Response({ status: 400 });
-
-  // Contract transaction response.
   return c.contract({
     abi,
     chainId: `eip155:${baseSepolia.id}`,
@@ -85,6 +44,48 @@ app.transaction('/mint/:id', (c) => {
     to: '0xc33D61f88f207D2c1E3E2e90cF389F9bfCb25C2a',
   });
 });
+
+app.frame('/finish/:id', (c) => {
+  return c.res({
+    image: (
+      <div style={{
+        color: 'white', 
+        display: 'flex', 
+        justifyContent: 'center', // Centers content horizontally
+        alignItems: 'center', // Centers content vertically
+        fontSize: '40px', 
+        height: '100%', // Make sure the div takes full height of its container
+        width: '100%', // Make sure the div takes full width of its container
+        backgroundColor: 'black', // Background color for visibility
+        flexDirection: 'column', // Stack children vertically
+        textAlign: 'center', // Center text lines horizontally
+      }}>
+        Minted!
+        <br />
+        Follow the /cast-clouds<br />channel for more free mints
+      </div>
+    ),
+    imageAspectRatio: '1:1',
+    intents: [
+      <Button.Link href="https://warpcast.com/~/channel/cast-clouds">/cast-clouds</Button.Link>,
+    ]
+  });
+});
+
+
+
+// app.frame('/mintbutton', (c) => {
+//   const { buttonValue } = c;
+//   console.log("mintbutton");
+//   return c.res({
+//     // image: `${process.env.NEXT_PUBLIC_SITE_URL}/cast-cloud-daos.png`,
+//     image: "https://i.imgur.com/0VNw4fC.png",
+//     imageAspectRatio: '1:1',
+//     intents: [
+//       <Button.Mint target="eip155:84532:0xc33D61f88f207D2c1E3E2e90cF389F9bfCb25C2a:0">Mint</Button.Mint>,
+//     ]
+//   })
+// })
 
 // app.frame('/finish', (c) => {
 //   console.log("finish: ", c);
